689314c715c7718eb484908d71f65f77c746675d
Brett Celestre <bcelestre@gmail.com>
2016-11-16 15:27:06 -0800

    Completes linkedList hasCycle function

--- problems/linkedLists/prompts/cycle.js
+++ problems/linkedLists/prompts/cycle.js
@@ -15,7 +15,30 @@
  */
 
 const hasCycle = linkedList => {
-  // TODO: Implement linked 
+  // Stores last value during subRoutine
+  let lastValue;
+  // subRoutine function takes in a node
+  const subRoutine = node => {
+    // Check if next value is not null
+    if ( node.next !== null ) {
+      // Then check if the last value equals the next values value
+      if ( lastValue === node.next.value ) {
+        // You found a cycle, return true
+        return true;
+      } else {
+        // Updates lastValue with current value
+        lastValue = node.value;
+        // Invokes subRoutine with next node
+        return subRoutine(node.next);
+      }
+    // Checks if the next value is null && the current value equals the tail value
+    } else if ( node.next === null  && node.value === linkedList.tail.value ) {
+      // There is no cycle, return false
+      return false;
+    }
+  };
+  // Invoke subRoutine with the linkedList's head
+  return subRoutine(linkedList.head);
 };
 
 module.exports = { hasCycle };
