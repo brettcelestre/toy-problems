a169a24bed10974a6985db891b1860579497c8d5
Brett Celestre <bcelestre@gmail.com>
2017-01-08 18:33:23 -0800

    Compeletes contains and adds more tests

--- problems/trees/prompts/basic_tree.js
+++ problems/trees/prompts/basic_tree.js
@@ -9,16 +9,20 @@
 class Tree {
   constructor(val) {
     this.value = val
+    this.parent = null
     this.children = []
   }
 
   addChild(val) {
     this.children.push(new Tree(val));
+    return true;
   }
 
   contains(target) {
-    
+    return (this.value === target) ? true :
+    this.children.some(child => child.contains(target))
   }
+   
 };
 
 module.exports = { Tree };
--- problems/trees/tests/basic_tree.test.js
+++ problems/trees/tests/basic_tree.test.js
@@ -1,30 +1,47 @@
 'use strict';
 
 const solution = require('../prompts/basic_tree.js'),
-      assert = require('chai').assert;
+      assert = require('chai').assert,
+      expect = require('chai').expect;
+		
 
-describe('trees', function() {
-	describe('basic tree', function (){
+describe('Trees', function() {
+	
+	describe('Basic tree', function (){
+		
+		let tree;
+		
+		beforeEach(function() {
+	    tree = new solution.Tree();
+	  });
+		
+		it('should have a method named "contains"', function() {
+	    expect(tree.contains).to.be.a('function');
+	  });
+	  
+	  it('should return true when adding a children to the tree', function(){
+	    expect(tree.addChild(5)).to.equal(true);
+	  });
 
 	  it('should add children to the tree', function() {
-	    const tree = new solution.Tree();
 	    tree.addChild(5);
-	    console.log('tree: ', tree);
 	    assert.equal(tree.children[0].value, 5);
 	  });
 
 	  it('should find contained values', function(){
-	    const tree = new solution.Tree();
 	    tree.addChild(5);
 	    assert.isTrue(tree.contains(5));
 	  });
 
 	  it('should add grandchildren', function() {
-	    const tree = new solution.Tree();
 	    tree.addChild(5);
 	    tree.children[0].addChild(6);
-	    console.log('tree: ', tree);
-	    assert.equal(tree.children[0].children[0].value, 6);
+	    assert.isTrue(tree.contains(6));
+	  });
+	  
+	  it('should return false for a value that was not added', function() {
+	    tree.addChild(5);
+	    expect(tree.contains(6)).to.equal(false);
 	  });
 
 	});
