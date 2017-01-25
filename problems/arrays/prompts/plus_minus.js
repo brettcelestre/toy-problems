848858c7a8ab27d79edd39983402dd9771fcd2f4
Brett Celestre <bcelestre@gmail.com>
2017-01-24 19:45:39 -0800

    Sets up Array > Plus Minus with test file

---
+++ problems/arrays/prompts/plus_minus.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Given an array of integers, calculate which fraction of its elements are positive, which fraction of its elements are negative, and which fraction of its elements are zeroes, respectively. Print the decimal value of each fraction on a new line.
+
+// Input Format:
+// The first line contains an integer, , denoting the size of the array. 
+// The second line contains  space-separated integers describing an array of numbers .
+
+/* Sample Input: 
+6
+-4 3 -9 0 4 1 
+*/
+
+/* You must print the following  lines:
+1) A decimal representing of the fraction of positive numbers in the array.
+2) A decimal representing of the fraction of negative numbers in the array.
+3) A decimal representing of the fraction of zeroes in the array.
+*/
+
+/* Sample Output:
+0.500000
+0.333333
+0.166667
+*/
+
+const plusMinus = (size, arr) => {
+};
+
+module.exports = { plusMinus };
---
+++ problems/arrays/tests/plus_minus.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const solution = require('../prompts/plus_minus.js');
+const assert = require ('chai').assert;
+
+describe('Arrays', () => {
+  describe('Plus Minus', () => {
+
+    it('...', () => {
+      // assert.equal(solution.every([1,2,3,4,4,5], element => {
+      //   return typeof element === 'number';
+      // }), true);
+    });
+
+    it('...', () => {
+      // assert.equal(solution.every([1,2,3,4,'hello!'], element => {
+      //   return typeof element === 'number';
+      // }), false);
+    });
+
+  });
+});
