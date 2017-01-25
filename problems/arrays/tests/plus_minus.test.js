bf14d8838e0a602b3f3178a120b6943141f48a40
Brett Celestre <bcelestre@gmail.com>
2017-01-24 20:09:30 -0800

    Adds basic tests to Plus Minus

--- problems/arrays/tests/plus_minus.test.js
+++ problems/arrays/tests/plus_minus.test.js
@@ -2,21 +2,27 @@
 
 const solution = require('../prompts/plus_minus.js');
 const assert = require ('chai').assert;
+const expect = require ('chai').expect;
 
 describe('Arrays', () => {
   describe('Plus Minus', () => {
 
-    it('...', () => {
-      // assert.equal(solution.every([1,2,3,4,4,5], element => {
-      //   return typeof element === 'number';
-      // }), true);
+    it('should be a function', () => {
+      expect(solution.plusMinus).to.be.a('function');
     });
 
-    it('...', () => {
-      // assert.equal(solution.every([1,2,3,4,'hello!'], element => {
-      //   return typeof element === 'number';
-      // }), false);
+    it('should return an array', () => {
+      expect(solution.plusMinus(6, [-4,3,-9,0,4,1])).to.be.a('array');
     });
+    
+    it('should return an array with a length of 3', () => {
+      let answer = solution.plusMinus(6, [-4,3,-9,0,4,1]);
+      expect(answer.length).to.be.equal(3);
+    });
+    
+    // it('...', () => {
+    //   expect(solution.plusMinus(6, [-4,3,-9,0,4,1])).to.be.a('array');
+    // });
 
   });
 });
