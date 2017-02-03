e6230ae272a2564d6ca41d0dcdc74fc7f31aae57
Brett Celestre <bcelestre@gmail.com>
2017-02-02 17:55:18 -0800

    Completes snake_case_converter function

--- problems/strings/prompts/snake_case_converter.js
+++ problems/strings/prompts/snake_case_converter.js
@@ -1,7 +1,18 @@
 // camelCase to snake_case
 
 const snakeCaseConverter = (str) => {
-  return str;
+  if ( typeof str === 'string' ) {
+    let x = str.split(''), result = '';
+    x.forEach(val => {
+      if ( val === val.toUpperCase() ) {
+        result += '_' + val.toLowerCase();
+      } else {
+        result += val;
+      } 
+    });
+    return result;
+  }
+  return 'Input must be a string';
 };
 
 module.exports = { snakeCaseConverter };
