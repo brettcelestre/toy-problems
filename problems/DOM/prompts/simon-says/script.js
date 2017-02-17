6038b308deb206fda7bcd924400a29bb4a068370
Brett Celestre <bcelestre@gmail.com>
2017-02-17 10:39:50 -0800

    Cleans up code

--- problems/DOM/prompts/simon-says/script.js
+++ problems/DOM/prompts/simon-says/script.js
@@ -19,6 +19,16 @@ document.addEventListener('DOMContentLoaded', function() {
     }); 
   }
   
+  const shuffleArray = (array) => {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+  
   // Checks to see if users pattern was correct
   const userPatternCheck = () => {
     let flag = true;
@@ -71,17 +81,8 @@ document.addEventListener('DOMContentLoaded', function() {
   
   // Starts Game
   simonStartColors(shuffleArray(['red', 'yellow', 'blue', 'green']));
+
   
 });
-
-const shuffleArray = (array) => {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
-};
 
 // module.exports = { simonSays };
