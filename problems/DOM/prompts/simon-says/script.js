93dd0751fd69cfbc5b141a3e7625f57d5546a22d
Brett Celestre <bcelestre@gmail.com>
2017-02-16 16:03:38 -0800

    Creates shuffled array

--- problems/DOM/prompts/simon-says/script.js
+++ problems/DOM/prompts/simon-says/script.js
@@ -10,10 +10,26 @@ document.addEventListener('DOMContentLoaded', function() {
     }); 
   }
   
+  
+  console.log('colors: ', shuffleArray(['red', 'yellow', 'blue', 'green']));
+  
 });
 
-const simonSays = () => {
-  
+
+const shuffleArray = (array) => {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+const simonVerifier = (simonsOrder, usersOrder) => {
+  // check if they are the same
+  // Show win state
+  // Ask to play again
 };
 
 module.exports = { simonSays };
